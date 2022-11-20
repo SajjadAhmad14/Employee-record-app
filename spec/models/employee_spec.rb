0ea3416b2ef20a2ffdd4cc1cacfd125da5f61e3a
Sajjad Ahmad <jogimar14@gmail.com>
2022-11-20 18:32:43 +0500

    Add model specs foy employment model

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -4,97 +4,61 @@ RSpec.describe Employee, type: :model do
   
   it "Employee should not be created if first name is blank" do
     employee = Employee.create(first_name: "", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "a@a.com", phone: "099212345")  
     expect(employee).not_to be_valid
   end
   
   it "Employee should be created if first name is not blank" do
     employee = Employee.create(first_name: "test", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "a@a.com", phone: "099212345")  
     expect(employee).to be_valid
   end
   
   it "Employee first name characters are more than 25" do
     employee = Employee.create(first_name: "testtesttesttesttesttesttest", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")
+      email: "a@a.com", phone: "099212345")
     expect(employee).to_not be_valid
   end
   
   it "Employee should not be created if last name is blank" do
     employee = Employee.create(first_name: "test", last_name: "", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "a@a.com", phone: "099212345")  
     expect(employee).not_to be_valid
   end
   
   it "Employee should be created if last name is not blank" do
     employee = Employee.create(first_name: "test", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "a@a.com", phone: "099212345")  
     expect(employee).to be_valid
   end
   
   it "Employee last name characters are more than 50" do
     employee = Employee.create(first_name: "testtesttesttesttesttesttesttesttesttesttesttesttesttest", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")
+      email: "a@a.com", phone: "099212345")
     expect(employee).to_not be_valid
   end
 
   it "Employee should not be created if email is blank" do
     employee = Employee.create(first_name: "test", last_name: "last test", nick_name: "test",
-      email: "", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "", phone: "099212345")  
     expect(employee).not_to be_valid
   end
 
   it "Employee should be created if email is not blank" do
     employee = Employee.create(first_name: "test", last_name: "lat test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "a@a.com", phone: "099212345")  
     expect(employee).to be_valid
   end
 
   it "Employee should not be created if phone is blank" do
     employee = Employee.create(first_name: "test", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "a@a.com", phone: "")  
     expect(employee).not_to be_valid
   end
 
   it "Employee should be created if phone is not blank" do
     employee = Employee.create(first_name: "test", last_name: "lat test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
-    expect(employee).to be_valid
-  end
-
-  it "Employee should not be created if employer is blank" do
-    employee = Employee.create(first_name: "test", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
-    expect(employee).not_to be_valid
-  end
-
-  it "Employee should be created if employer is not blank" do
-    employee = Employee.create(first_name: "test", last_name: "lat test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
-    expect(employee).to be_valid
-  end
-
-  it "Employee should not be created if date started is blank" do
-    employee = Employee.create(first_name: "test", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "", date_employment_ended: "2022-02-05")  
-    expect(employee).not_to be_valid
-  end
-
-  it "Employee should be created if employer is not blank" do
-    employee = Employee.create(first_name: "test", last_name: "lat test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
-    expect(employee).to be_valid
-  end
-
-  it "Employee should not be created if date employment ended is blank" do
-    employee = Employee.create(first_name: "test", last_name: "last test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "", date_employment_ended: "")  
-    expect(employee).not_to be_valid
-  end
-
-  it "Employee should be created if date employment ended is not blank" do
-    employee = Employee.create(first_name: "test", last_name: "lat test", nick_name: "test",
-      email: "a@a.com", employer: "test employer", phone: "099212345", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+      email: "a@a.com", phone: "099212345")  
     expect(employee).to be_valid
   end
 end
--- spec/models/employment_spec.rb
+++ spec/models/employment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Employment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Employment should not be created if employer is blank" do
+    employment = Employment.create(employer: "", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+    expect(employment).not_to be_valid
+  end
+
+  it "Employment should be created if employer is not blank" do
+    employment = Employment.create(employer: "test employer", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+    expect(employment).to be_valid
+  end
+
+  it "Employment should not be created if date started is blank" do
+    employment = Employment.create(employer: "test employer", date_started: "", date_employment_ended: "2022-02-05")  
+    expect(employment).not_to be_valid
+  end
+
+  it "Employment should be created if date started is not blank" do
+    employment = Employment.create(employer: "test employer", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+    expect(employment).to be_valid
+  end
+
+  it "Employment should not be created if date employment ended is blank" do
+    employment = Employment.create(employer: "test employer", date_started: "2020-12-20", date_employment_ended: "")  
+    expect(employment).not_to be_valid
+  end
+
+  it "Employment should be created if date employment ended is not blank" do
+    employment = Employment.create(employer: "test employer", date_started: "2020-12-20", date_employment_ended: "2022-02-05")  
+    expect(employment).to be_valid
+  end
 end
