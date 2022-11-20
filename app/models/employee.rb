0df7887c778b957bc0322e1931f03ebcc6802f21
Sajjad Ahmad <jogimar14@gmail.com>
2022-11-20 22:41:36 +0500

    Change style for personal data form

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,4 @@ class Employee < ApplicationRecord
   validates_length_of :last_name, maximum: 50, allow_blank: false
   validates :email, presence: true
   validates :phone, presence: true
-  validates :employer, presence: true
-  validates :date_started, presence: true
-  validates :date_employment_ended, presence: true
 end
