78fd0c99e646960bb7c0ff780427504e09e9d4dd
Sajjad Ahmad <jogimar14@gmail.com>
2022-11-26 06:49:07 +0500

    Add disable functionality to save btns

--- app/javascript/controllers/employment_controller.js
+++ app/javascript/controllers/employment_controller.js
@@ -6,4 +6,19 @@ export default class extends Controller {
   validateDate(e) {
     e.target.value=e.target.value.replace(/^(\d\d)(\d)$/g,'$1/$2').replace(/^(\d\d\/\d\d)(\d+)$/g,'$1/$2').replace(/[^\d\/]/g,'')
   }
+
+  enableSaveBtn() {
+    let btn = document.querySelector(".save-btn-employment");
+    let reuiredFields = document.getElementsByClassName("required-field-emp");
+    console.log(reuiredFields)
+    let isValid = true;
+    for (var i = 0; i < reuiredFields.length; i++) {
+      let newField = reuiredFields[i];
+      if (newField.value.trim() === "" || newField.value === null) {
+        isValid = false;
+        break;
+      }
+    }
+    btn.disabled = !isValid;
+  }
 }
--- app/javascript/controllers/personal_controller.js
+++ app/javascript/controllers/personal_controller.js
@@ -4,8 +4,25 @@ export default class extends Controller {
   static targets = ["content"];
 
   validatePhone(e) {
-    if (e.key != "Backspace" && (e.target.value.length === 3 || e.target. value.length === 7)) {
+    if (
+      e.key != "Backspace" &&
+      (e.target.value.length === 3 || e.target.value.length === 7)
+    ) {
       e.target.value += "-";
     }
+  }
+
+  enableSaveBtn() {
+    let btn = document.querySelector(".save-btn-personal");
+    let reuiredFields = document.getElementsByClassName("required-field");
+    let isValid = true;
+    for (var i = 0; i < reuiredFields.length; i++) {
+      let newField = reuiredFields[i];
+      if (newField.value.trim() === "" || newField.value === null) {
+        isValid = false;
+        break;
+      }
+    }
+    btn.disabled = !isValid;
   }
 }
