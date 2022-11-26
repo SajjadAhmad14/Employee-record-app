1b1b58211b2115e500c140ae7468659a081a7cf3
Sajjad Ahmad <jogimar14@gmail.com>
2022-11-26 05:42:50 +0500

    Allow user to input date manuaaly as well as select from calendar

--- app/controllers/employments_controller.rb
+++ app/controllers/employments_controller.rb
@@ -4,8 +4,11 @@ class EmploymentsController < ApplicationController
   end
 
   def create
-    p employment_params
-    @employment = Employment.new(employment_params)
+    if employment_params[:date_started] != '' && employment_params[:date_employment_ended] != ''
+      @employment = Employment.new(employer: employment_params[:employer], date_started:employment_params[:date_started], date_employment_ended: employment_params[:date_employment_ended])
+    else
+      @employment = Employment.new(employment_params)
+    end
     if @employment.save
       redirect_to controller: :home, action: :home_page, notice: "Employment is created successfully."
     else
